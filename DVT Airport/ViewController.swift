e9cee212ff1490fcef73995c497e1ed36af8a7b8
Koza, Sipho (RBI-UK) <sipho.koza@reedbusiness.com>
2019-06-18 15:03:16 +0200

    Created the DVT Airport app and added the icons that were provided.

---
+++ DVT Airport/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DVT Airport
+//
+//  Created by Koza, Sipho (RBI-UK) on 18/06/2019.
+//  Copyright © 2019 Koza, Sipho (RBI-UK). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
